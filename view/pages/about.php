e6918e981af089ed8d0883d99347ed2ee621047e
Ali Al-Wazny <ali@pop-os.localdomain>
2023-07-17 20:18:54 +0200

    feat: Add styling file

--- view/pages/about.php
+++ view/pages/about.php
@@ -4,11 +4,47 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../assets/styles/style.css">
     <title>Document</title>
+
+    <style>
+        main {
+            background-image: url('../../assets/images/IMG-9179.jpg');
+        }
+    </style>
 </head>
 <body>
-    remove burger menu animation when a page is done loading
+    <!-- remove burger menu animation when a page is done loading
     side nav underline animation
-    language switcher
+    language switcher -->
+    <main>
+        <h1>About me</h1>
+        <div>
+        Moin! 
+
+Ich bin Julia, 24 Jahre alt und bin vor 2 Jahren nach Karlsruhe gezogen.
+
+Insgesamt tätowiere ich mittlerweile seit ca
+
+8 Jahren, damit richtig angefangen habe ich mit 16. Um mich abzusichern hatte ich nebenher eine Ausbildung zur Kosmetikerin angefangen, die mich aber leider dann nicht annähernd so gepackt hat... irgendwie war mir das zu oberflächlich.
+
+Im Gegensatz dazu ist das Tätowieren für mich im Allgemeinen ein sehr intimer Prozess. Zu schätzen weiß ich, wie sehr dieser auf Vertrauen, Sympathie und einer klaren Kommunikation basiert. Ich bin froh einen Beruf ausüben zu dürfen, welcher mich täglich dazu bringt, mein volles Potential zu entfalten und mich in vielen
+
+Aspekten mit Dankbarkeit und Stolz belohnt.
+
+Herausforderungen und Neues reizen mich, so manche würden mich deshalb als Allrounder bezeichnen - doch am liebsten arbeite ich mit einer Mischung aus Stilrichtungen, wie Neotraditional, Japanese, Traditional und Blackwork.
+
+Ebenso gerne greife ich Elemente aus dem
+
+Jugendstil auf.
+
+Auf das gekonnte Zusammenspiel von kräftigen Linien und sanften Schatten lege ich großen Wert und es ist Teil meiner persönlichen Handschrift. Das
+
+Ineinandergreifen beider Stilelemente lässt in jedem meiner Werke einen ganz
+
+individuellen Charme entstehen.
+        </div>
+    </main>
+    <?php include("./pagelets/navigation.php"); ?>
 </body>
 </html>
