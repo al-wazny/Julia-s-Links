c98636d39a55a56dfe4c918a35d97ace07854821
Ali Al-Wazny <ali@pop-os.localdomain>
2023-07-26 19:20:39 +0200

    release v0.2.4

--- view/pages/index.php
+++ view/pages/index.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="google-site-verification" content="4bZPjSHZU1qh-NWF8JIz_o9Ivz9tKf4cbiSJREBy4bE" />
     <link rel="stylesheet" href="../../assets/styles/style.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
